adff46e0bd930cd8a89a67804c50f809db8b63db
JoshKarpel <josh.karpel@gmail.com>
2019-08-04 15:08:53 -0500

    foobar

--- colors/colors.py
+++ colors/colors.py
@@ -1,10 +1,6 @@
 START_SEQUENCES = {
     'red': '\u001b[31m',
     'green': '\u001b[32m',
-    'blue':'\u001b[34m',
+    'blue': '\u001b[34m',
 }
 RESET_SEQUENCE = '\u001b[0m'
-
-print(START_SEQUENCES['green'], end = '')
-print('Hello world!', end = '')
-print(RESET_SEQUENCE)
